80c667e55e41e7d9bc14f35c6e687375b2e3832c
Book-reader <thevoid@outlook.co.nz>
2024-05-11 10:54:05 +1200

    initial hyprland setup

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "Building iso image"
+mkdir ./iso-output
+sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer \
+ghcr.io/jasonn3/build-container-installer:latest

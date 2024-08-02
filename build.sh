1e8cbffe921816483ca88ead4235af6efe28d48a
Book-reader <89329785+Book-reader@users.noreply.github.com>
2024-08-02 22:42:42 +1200

    Add VARIANT to build.sh (untested)
    This should fix not having a VARIANT  in os-release (breaks topgrade immutable detection) and obsolete the hack of appending it in recipie.yml
    However this will only work if I reinstall my OS with the new iso.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 echo "Building iso image"
 mkdir ./iso-output
 sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer \
-ghcr.io/jasonn3/build-container-installer:latest
+ghcr.io/jasonn3/build-container-installer:latest VARIANT=Silverblue
